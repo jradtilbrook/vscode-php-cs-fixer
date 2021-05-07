d7a5f3c14e4bc26da09729d855e9fcb189d206c9
jradtilbrook <jradtilbrook@hey.com>
2021-05-07 15:15:28 +0800

    Some cleanup of code and docs

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
 import * as vscode from 'vscode';
 import { spawn } from 'child_process';
-import { homedir } from 'os';
 import { applyPatch } from 'diff';
 import * as path from 'path';
 import * as fs from 'fs';
@@ -20,7 +19,7 @@ interface PHPCSFixerDiff {
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate() {
     vscode.languages.registerDocumentFormattingEditProvider('php', {
         provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.ProviderResult<vscode.TextEdit[]> {
             return new Promise<vscode.TextEdit[]>((resolve, reject) => {
@@ -68,17 +67,16 @@ export function activate(context: vscode.ExtensionContext) {
                     fixer.stdout.on('end', () => {
                         try {
                             let json: PHPCSFixerOutput = JSON.parse(rawOutput);
-                            console.log(json.files[0]?.diff ?? 'no diff');
                             // get the whole document range to apply over
                             const lastLine = document.lineAt(document.lineCount - 1).range.end;
                             const range = new vscode.Range(document.positionAt(0), lastLine);
 
                             // apply the generate diff patch from php-cs-fixer to the whole document
                             let output = applyPatch(document.getText(), json.files[0]?.diff ?? '');
-                            console.log(output);
 
                             return resolve([new vscode.TextEdit(range, output)]);
                         } catch (error) {
+                            console.log(error);
                             return reject(error);
                         }
                     });
